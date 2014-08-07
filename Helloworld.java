0a6551b4456b3230dd282e2fe896149e843a514e
vishnuRed <vishnu.virtual@gmail.com>
2014-08-07 12:12:05 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,10 @@
 public class Helloworld {
 public static void main(String[] args){
 System.out.println("HELL0 world");
+Helloworld helloworld = new Helloworld();
+helloworld.sum(41,4);
+}
+public void sum(int a, int b){
+  System.out.println("SUM of "+a+" and "+b+" is "+(a+b) );
 }
 }
